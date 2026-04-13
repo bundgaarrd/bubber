c38b9e71905b62a6fd87963691ebe02a1660a01a
Ahmed-ai2 <s246522@dtu.dk>
2026-04-13 16:29:35 +0200

    new files

--- src/main/java/appLogic/Customer.java
+++ src/main/java/appLogic/Customer.java
@@ -1,4 +1,35 @@
 package appLogic;
 
 public class Customer {
+    private String name;
+    private String email;
+    private String phoneNumber;
+
+    public Customer(String name, String email, String phoneNumber){
+        this.name = name;
+        this.email = email;
+        this.phoneNumber = phoneNumber;
+    }
+
+    public boolean verifyIsEmail(String email){
+
+        if(!email.isEmpty()){
+            return false;
+        }
+        if (!email.contains("@")) {
+            return false;
+
+
+        }
+        if(!email.contains(".")){
+            return false;
+        }
+        
+        return true;
+    
+
+    }
+
+
+
 }
