cd5d6fce35f3df649c4de29069160ee07fe28527
Andreas Bundgaard <89105860+palmer312@users.noreply.github.com>
2026-04-13 15:50:26 +0200

    Added attributes

--- src/main/java/appLogic/Project.java
+++ src/main/java/appLogic/Project.java
@@ -1,14 +1,29 @@
 package appLogic;
 
+import java.util.Set;
+
 public class Project {
 
 //    public static void main(String[] args) {
 //
 //    }
 
+    private String projectID;
+    private String name;
+
+    private Integer expectedHours;
+    private boolean hasCustomer;
+    private boolean isCompleted = false;
+    private Employee projectLeader;
+
+    private Set<Activity> activities;
+
+
+
     // Constructor
     public Project(String name, Integer expectedHours) {
-
+        this.name = name;
+        this.expectedHours = expectedHours;
     }
 
 }
