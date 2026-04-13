cc0ba5c2bd7efb7d6d762e5383f0da3acb95ec25
Andreas Bundgaard <89105860+palmer312@users.noreply.github.com>
2026-04-13 15:21:15 +0200

    Added project

---
+++ src/main/java/appLogic/Project.java
@@ -0,0 +1,14 @@
+package appLogic;
+
+public class Project {
+
+//    public static void main(String[] args) {
+//
+//    }
+
+    // Constructor
+    public Project(String name, Integer expectedHours) {
+
+    }
+
+}
