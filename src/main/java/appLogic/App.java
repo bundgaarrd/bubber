21a7344701de2d55d5b3ea59097c38189a4925b5
leac-1 <s245072@dtu.dk>
2026-04-13 17:26:47 +0200

    App class

---
+++ src/main/java/appLogic/App.java
@@ -0,0 +1,57 @@
+package appLogic;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
+
+public class App {
+    private Map<String, Employee> employees;
+    private Set<Project> projects;
+    private boolean adminLogin;
+
+    public App() {
+        this.employees = new HashMap<>();
+        this.projects = new HashSet<>();
+        this.adminLogin = false;
+    }
+
+    public Project Project(String name) { // Create project
+        Project newProject = new Project(name);
+        this.projects.add(newProject);
+        return newProject;
+    }
+
+    public void deleteProject(String id) {
+        projects.removeIf(project -> project.projectID().equals(id)); // Antager at Project har en projectID() metode
+    }
+
+    public List<Employee> getAvailableEmployees(int week, int year) {
+        return null; // placeholder 
+        // Hvordan skal det laves?
+        // Hvornår er en employee "available"? Under hvilket antal timer på activities?
+    }
+
+    public Project getProjectById(String id) {
+        for (Project p : projects) {
+            if (p.projectID().equals(id)) {
+                return p;
+            }
+        }
+    return null;
+    }
+
+    public void importEmployeesFromFile(String path) {
+    }
+
+    public List<Project> getAllProjects() {
+        return new ArrayList<>(projects);
+    }
+
+    public boolean isAdminLoggedIn() {
+        return adminLogin;
+    }
+
+}
