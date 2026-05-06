972eea8a97bd697c4670af1032da30c4ac81140f
Andreas Bundgaard <89105860+palmer312@users.noreply.github.com>
2026-05-06 11:58:45 +0200

    one can now add a project from the UI

--- src/main/java/ui/ChooseProjectView.java
+++ src/main/java/ui/ChooseProjectView.java
@@ -1,20 +1,17 @@
 package ui;
 
 import appLogic.App;
+import appLogic.employee.EmployeeRepository;
 import appLogic.project.Project;
 import javafx.geometry.Insets;
 import javafx.geometry.Pos;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
-import javafx.scene.control.Button;
+import javafx.scene.control.*;
 import javafx.scene.layout.VBox;
 
 import javafx.scene.Parent;
 import javafx.scene.Scene;
-import javafx.scene.control.Label;
-import javafx.scene.control.TableColumn;
-import javafx.scene.control.TableView;
-import javafx.scene.control.TextField;
 import javafx.scene.layout.BorderPane;
 import javafx.scene.layout.GridPane;
 import javafx.collections.FXCollections;
@@ -24,15 +21,15 @@ public class ChooseProjectView {
 
     private Scene scene;
     private ObservableList<Project> projectData = FXCollections.observableArrayList();
+    private EmployeeRepository employeeRepository;
+    private App app;
 
     public ChooseProjectView(Scene scene) {
         this.scene = scene;
+        this.app = App.getInstance();
     }
 
     public Parent getView() {
-        // App calls
-        App app = App.getInstance();
-
         BorderPane root = new BorderPane();
         GridPane grid = new GridPane();
         grid.setPadding(new Insets(15));
@@ -40,9 +37,9 @@ public class ChooseProjectView {
         grid.setHgap(10);
         grid.setVgap(10);
 
-        // -----Tables-----
-        TableView<Project> tableView = new TableView<>();
-        tableView.setItems(projectData);
+        // -----Begin tables-----
+        TableView<Project> tableData = new TableView<>();
+        tableData.setItems(projectData);
 
         TableColumn<Project, String> nameCol = new TableColumn<>("Project name");
         nameCol.setCellValueFactory(data ->
@@ -53,25 +50,45 @@ public class ChooseProjectView {
         idCol.setCellValueFactory(data ->
                 new javafx.beans.property.SimpleStringProperty(data.getValue().getProjectID()));
 
-        tableView.getColumns().addAll(nameCol, idCol);
+        tableData.getColumns().addAll(nameCol, idCol);
 
-        // For later
-        // projectData.addAll(ProjectRepository.getAllProjects())
+        projectData.addAll(app.getProjectRegistry().getAllProjects());
 
         //-----End tables
 
+        // -----Begin buttons------
         Button backBtn = new Button("Back");
         grid.add(backBtn, 1, 0);
 
-        // ActionEvents for interactive elements
+        Button addProjectBtn = new Button("Add project");
+        grid.add(addProjectBtn,2,0);
+
+        // -----End buttons------
+
+        // -----Begin ActionEvents for interactive elements-----
         backBtn.setOnAction(e -> {
             MainView mainView = new MainView(scene);
             scene.setRoot(mainView.getView());
         });
 
+        addProjectBtn.setOnAction(e -> {
+            TextInputDialog dialog = new TextInputDialog();
+            dialog.setTitle("Add project information");
+            dialog.setHeaderText(null);
+            dialog.setContentText("Project name");
+
+            dialog.showAndWait().ifPresent(name -> {
+                if (!name.isBlank()) {
+                    Project project = app.getProjectRegistry().createProject(name);
+                    projectData.add(project);
+                }
+            });
+        });
+        // -----End ActionEvents for interactive elements-----
+
         // Layout adjustments
         root.setTop(grid);
-        root.setCenter(tableView);
+        root.setCenter(tableData);
         return root;
     }
 
