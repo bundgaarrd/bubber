13c1756cfae714a157735e35d65b7f6f00471909
Stefan Artur Stefirta <128942076+SAStef@users.noreply.github.com>
2026-05-08 13:07:48 +0200

    ic

--- src/main/java/appLogic/activity/command/CreateProjectActivity.java
+++ src/main/java/appLogic/activity/command/CreateProjectActivity.java
@@ -1,7 +1,6 @@
 package appLogic.activity.command;
 
 import java.time.LocalDate;
-import java.util.UUID;
 
 public record CreateProjectActivity(String projectId, String name, String description, String summary, LocalDate date) {
 }
