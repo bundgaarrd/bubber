63aa40f04b6373218d902816bdcc9ecaaf3ff93b
ValdemarF20 <valdemarfoster.dk@gmail.com>
2026-05-08 09:33:43 +0200

    move to AppContext usage

--- src/main/java/appLogic/App.java
+++ src/main/java/appLogic/App.java
@@ -1,10 +1,9 @@
 package appLogic;
 
+import appLogic.activity.ActivityService;
 import appLogic.employee.Employee;
 import appLogic.employee.EmployeeRepository;
-import appLogic.employee.InMemoryEmployeeRepository;
 import appLogic.employee.InMemoryTimeEntryRepository;
-import appLogic.activity.*;
 import appLogic.project.Project;
 import appLogic.project.ProjectRegistry;
 
@@ -12,12 +11,8 @@ import java.util.*;
 
 public class App {
     private static App instance;
+    private AppContext appContext;
 
-    private final ProjectRegistry projectRegistry;
-    private final EmployeeRepository employeeRepository;
-    private final InMemoryTimeEntryRepository timeEntryRepository;
-    private final ActivityService activityService;
-    private final ActivityRepository activityRepository;
     private Employee loggedInUser;
 
     public static void main(String[] args) { // Has to be run from mvn javafx:run
@@ -30,40 +25,15 @@ public class App {
     }
 
     private App() {
-        this.employeeRepository = new InMemoryEmployeeRepository();
-        this.projectRegistry = new ProjectRegistry();
-        this.timeEntryRepository = new InMemoryTimeEntryRepository();
-        this.activityRepository = new InMemoryActivityRepository();
-        this.activityService = new DefaultActivityService(
-                this.activityRepository,
-                this.projectRegistry,
-                this::getLoggedInUser,
-                this.timeEntryRepository
-        );
-    }
-
-    private void initializeUsers() {
-        employeeRepository.save(new Employee("huba", "Hubert Baumeister", true));
-        employeeRepository.save(new Employee("wilo", "William Lopez", true));
-        employeeRepository.save(new Employee("anda", "Annemette A. Damgaard", true));
-        Employee laha = new Employee("laha", "Lars Hansen", true);
-        employeeRepository.save(laha);
-        Employee alla = new Employee("alla", "Allan Lassen", true);
-        employeeRepository.save(alla);
-
-        Project KBHShop = projectRegistry.createProject("KBHShop"); // generates 26001
-        KBHShop.assignProjectLeader(laha);
-
-        Project DTU = projectRegistry.createProject("DTU");         // generates 26002
-        DTU.assignProjectLeader(alla);
+        appContext = AppContext.initialize();
     }
 
     public static App getInstance() {
         if(instance == null) {
             instance = new App();
-            instance.initializeUsers();
             instance.run();
         }
+
         return instance;
     }
 
@@ -72,7 +42,7 @@ public class App {
     }
 
     public List<Employee> getAvailableEmployees() {
-        Map<String, Employee> employeeMap = employeeRepository.getEmployees();
+        Map<String, Employee> employeeMap = appContext.getEmployeeRepository().getEmployees();
         List<Employee> returnList = new ArrayList<>();
 
         for (Employee emp : employeeMap.values()) {
@@ -84,24 +54,16 @@ public class App {
         return returnList;
     }
 
-    public void importEmployeesFromFile(String path) {
+    public AppContext getAppContext() {
+        return appContext;
     }
 
     public void login(String initials) {
-        if (initials == null || initials.length() > 4) {
-            throw new IllegalArgumentException("Initials must be 1-4 characters");
-        }
-
-        Employee emp = employeeRepository.findByInitials(initials);
-        if (emp == null) {
-            throw new IllegalStateException("Employee not found");
-        }
-
-        this.loggedInUser = emp;
+        appContext.login(initials);
     }
 
     public EmployeeRepository getEmployeeRepository() {
-        return employeeRepository;
+        return appContext.getEmployeeRepository();
     }
 
     // Check login
@@ -114,28 +76,24 @@ public class App {
     }
 
     public ProjectRegistry getProjectRegistry() {
-        return projectRegistry;
+        return appContext.getProjectRegistry();
     }
 
     public ActivityService getActivityService() {
-        return activityService;
+        return appContext.getActivityService();
     }
 
     public InMemoryTimeEntryRepository getTimeEntryRepository() {
-        return timeEntryRepository;
-    }
-
-    public void testMethod() {
-        System.out.println("This is a testmethod from App.java\nThis means that the UI and app talks together");
+        return appContext.getTimeEntryRepository();
     }
 
     public Report generateReport(String projectId) {
-        Project project = projectRegistry.getProjectById(projectId);
+        Project project = getProjectRegistry().getProjectById(projectId);
         if (project == null) {
             throw new IllegalArgumentException("Project not found: " + projectId);
     }
-        List<Activity> activities = activityRepository.findByProject(projectId);
-        List<TimeEntry> allEntries = timeEntryRepository.findAll();
+        List<Activity> activities = getActivityService().findByProject(projectId);
+        List<TimeEntry> allEntries = getTimeEntryRepository().findAll();
 
         Set<Summary> summaries = new HashSet<>();
         double totalHoursUsed = 0;
