3966fb96d12b89c82cce2ff5dbfe95140d968a0f
ValdemarF20 <valdemarfoster.dk@gmail.com>
2026-05-10 18:28:55 +0200

    simplify report

--- src/main/java/appLogic/report/Report.java
+++ src/main/java/appLogic/report/Report.java
@@ -4,27 +4,4 @@ import java.util.Set;
 
 import appLogic.Summary;
 
-public class Report {
-    private double hoursUsed;
-    private Set<Summary> activitySummaries;
-    private double expectedRemainingHours;
-    
-    public Report(double hoursUsed, Set<Summary> activitySummaries, double expectedRemainingHours) {
-        this.hoursUsed = hoursUsed;
-        this.activitySummaries = activitySummaries;
-        this.expectedRemainingHours = expectedRemainingHours;
-    }
-
-    public double getHoursUsed() {
-        return hoursUsed;
-    }
-
-    public Set<Summary> getActivitySummaries() {
-        return activitySummaries;
-    }
-
-    public double getExpectedRemainingHours() {
-        return expectedRemainingHours;
-    }
-
-}
+public record Report(double hoursUsed, Set<Summary> activitySummaries, double expectedRemainingHours) {}
