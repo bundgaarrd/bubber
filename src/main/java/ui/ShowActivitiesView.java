16cf8c8417e2f9d9c983da915d4ebfca83310788
Andreas Bundgaard <89105860+palmer312@users.noreply.github.com>
2026-05-10 16:22:06 +0200

    c

--- src/main/java/ui/ShowActivitiesView.java
+++ src/main/java/ui/ShowActivitiesView.java
@@ -1,55 +1,43 @@
 package ui;
 
 import appLogic.App;
-import appLogic.TimeEntry;
-import appLogic.activity.ActivityRepository;
-import javafx.geometry.Pos;
-import javafx.scene.Parent;
-import javafx.scene.Scene;
-import javafx.scene.control.Button;
-import javafx.scene.control.Label;
-import javafx.scene.layout.VBox;
-
-import java.sql.Time;
-import java.util.ArrayList;
-import java.util.List;
-
-import appLogic.App;
 import appLogic.AppContext;
+import appLogic.activity.ActivityRepository;
+import appLogic.activity.impl.Activity;
 import appLogic.employee.Employee;
-import appLogic.employee.EmployeeRepository;
-import appLogic.project.Project;
-import appLogic.project.ProjectRegistry;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
-import javafx.event.ActionEvent;
 import javafx.geometry.Insets;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
-import javafx.scene.control.ButtonType;
-import javafx.scene.control.ChoiceBox;
-import javafx.scene.control.Dialog;
 import javafx.scene.control.Label;
 import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
-import javafx.scene.control.TextField;
 import javafx.scene.layout.BorderPane;
 import javafx.scene.layout.GridPane;
-import javafx.scene.layout.VBox;
+
+import java.util.Set;
 
 public class ShowActivitiesView {
 
     private Scene scene;
     private App app;
     private AppContext appContext;
-    private ObservableList<TimeEntry> entries = FXCollections.observableArrayList();
+    private ObservableList<Activity> entries = FXCollections.observableArrayList();
     private ActivityRepository activityRepository;
+    private Set<Activity> activitySet;
+    private Employee user;
+    private String username;
 
     public ShowActivitiesView(Scene scene) {
         this.scene = scene;
         this.app = App.getInstance();
         this.appContext = app.getAppContext();
+        this.user = appContext.getLoggedInUser();
+        this.username = this.user.getName();
+        this.activitySet = this.user.getActivities();
+        this.entries.addAll(activitySet);
     }
 
     public Parent getView() {
@@ -60,23 +48,19 @@ public class ShowActivitiesView {
         grid.setHgap(10);
 
         // Information in grid
-        Label message = new Label("Showing activies for user: " + appContext.getLoggedInUser().getName());
+        Label message = new Label("Showing activies for user: " + username);
         grid.add(message,1,1);
 
         Button backBtn = new Button("Back");
         grid.add(backBtn, 0, 1);
 
 
-
-
         // Columns
-        TableColumn<TimeEntry, String> activityNameCol = new TableColumn<>("Activity name");
+        TableColumn<Activity, String> activityNameCol = new TableColumn<>("Activity name");
         activityNameCol.setCellValueFactory(data ->
-                new javafx.beans.property.SimpleStringProperty(data.getValue().getActivity().getName()));
+                new javafx.beans.property.SimpleStringProperty(data.getValue().getDescription()));
 
-
-
-        TableView<TimeEntry> tableData = new TableView<>();
+        TableView<Activity> tableData = new TableView<>();
         tableData.setItems(entries);
         tableData.getColumns().addAll(activityNameCol);
 
@@ -85,10 +69,6 @@ public class ShowActivitiesView {
             MainView mainView = new MainView(scene);
             scene.setRoot(mainView.getView());
         });
-
-
-
-        // TODO: Set columns for entries
 
 
 
