9fe512d14f4b1daba7311dde64d2d9a7e0e980ce
Stefan Artur Stefirta <128942076+SAStef@users.noreply.github.com>
2026-05-11 20:31:52 +0200

    f

--- src/main/java/appLogic/activity/impl/FixedActivity.java
+++ src/main/java/appLogic/activity/impl/FixedActivity.java
@@ -1,10 +1,10 @@
 package appLogic.activity.impl;
 
-import appLogic.FixedActivityType;
-
 import java.time.LocalDateTime;
 import java.time.temporal.WeekFields;
 import java.util.Locale;
+
+import appLogic.FixedActivityType;
 
 public class FixedActivity extends Activity {
 
@@ -23,10 +23,12 @@ public class FixedActivity extends Activity {
         return type;
     }
 
+    @Override
     public LocalDateTime getStartDate() {
         return startDate;
     }
 
+    @Override
     public LocalDateTime getEndDate() {
         return endDate;
     }
